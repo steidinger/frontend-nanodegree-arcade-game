98fd19677382a170269080f24a772dd2da63c35d
Frank Steidinger <steidinger@acm.org>
2015-05-17 13:10:31 +0200

    feat: add 1 bug in each row moving at different speeds

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    this.width = 101;
+    this.x = 0;
+    this.y = 60 + (row - 1) * 83;
+    this.speed = speed || Enemy.defaultSpeed;
+};
+
+Enemy.defaultSpeed = 10;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,22 +20,32 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+    this.x += dt * this.speed;
+    if (this.x > 505) {
+        this.x = 0 - this.width;
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
+function nop() {}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(1, 20), new Enemy(2, 30), new Enemy(3, 10)]; // todo
+var player = {
+    update: nop,
+    render: nop,
+    handleInput: nop
+}; // todo
 
 
 // This listens for key presses and sends the keys to your
@@ -43,4 +59,8 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+});
+
+document.addEventListener("load", function () {
+    Engine.init();
 });
